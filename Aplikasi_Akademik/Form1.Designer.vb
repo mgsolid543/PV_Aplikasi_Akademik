91461492824a81fa0f7711545e89ce81bc408e36
Irpan Budiana <irpan.budiana@gmail.com>
2023-10-25 19:06:54 +0700

    first commit

---
+++ Aplikasi_Akademik/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FormUtama
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.JurusanToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.RuanganToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MataKuliahToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MahasiswaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DosenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.KeluarToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TransaksiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.KRSToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.KHSToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.LaporanToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CetakKRSToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.CetakKHSToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SettingToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PenggunaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BantuanToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InfoAplikasiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem, Me.TransaksiToolStripMenuItem, Me.LaporanToolStripMenuItem, Me.SettingToolStripMenuItem, Me.BantuanToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(1446, 40)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.JurusanToolStripMenuItem, Me.RuanganToolStripMenuItem, Me.MataKuliahToolStripMenuItem, Me.MahasiswaToolStripMenuItem, Me.DosenToolStripMenuItem, Me.ToolStripMenuItem1, Me.KeluarToolStripMenuItem})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(64, 36)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'JurusanToolStripMenuItem
+        '
+        Me.JurusanToolStripMenuItem.Name = "JurusanToolStripMenuItem"
+        Me.JurusanToolStripMenuItem.Size = New System.Drawing.Size(217, 36)
+        Me.JurusanToolStripMenuItem.Text = "&Jurusan"
+        '
+        'RuanganToolStripMenuItem
+        '
+        Me.RuanganToolStripMenuItem.Name = "RuanganToolStripMenuItem"
+        Me.RuanganToolStripMenuItem.Size = New System.Drawing.Size(217, 36)
+        Me.RuanganToolStripMenuItem.Text = "&Ruangan"
+        '
+        'MataKuliahToolStripMenuItem
+        '
+        Me.MataKuliahToolStripMenuItem.Name = "MataKuliahToolStripMenuItem"
+        Me.MataKuliahToolStripMenuItem.Size = New System.Drawing.Size(217, 36)
+        Me.MataKuliahToolStripMenuItem.Text = "Mata &Kuliah"
+        '
+        'MahasiswaToolStripMenuItem
+        '
+        Me.MahasiswaToolStripMenuItem.Name = "MahasiswaToolStripMenuItem"
+        Me.MahasiswaToolStripMenuItem.Size = New System.Drawing.Size(217, 36)
+        Me.MahasiswaToolStripMenuItem.Text = "&Mahasiswa"
+        '
+        'DosenToolStripMenuItem
+        '
+        Me.DosenToolStripMenuItem.Name = "DosenToolStripMenuItem"
+        Me.DosenToolStripMenuItem.Size = New System.Drawing.Size(217, 36)
+        Me.DosenToolStripMenuItem.Text = "&Dosen"
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(214, 6)
+        '
+        'KeluarToolStripMenuItem
+        '
+        Me.KeluarToolStripMenuItem.Name = "KeluarToolStripMenuItem"
+        Me.KeluarToolStripMenuItem.Size = New System.Drawing.Size(217, 36)
+        Me.KeluarToolStripMenuItem.Text = "Keluar"
+        '
+        'TransaksiToolStripMenuItem
+        '
+        Me.TransaksiToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.KRSToolStripMenuItem, Me.KHSToolStripMenuItem})
+        Me.TransaksiToolStripMenuItem.Name = "TransaksiToolStripMenuItem"
+        Me.TransaksiToolStripMenuItem.Size = New System.Drawing.Size(122, 36)
+        Me.TransaksiToolStripMenuItem.Text = "&Transaksi"
+        '
+        'KRSToolStripMenuItem
+        '
+        Me.KRSToolStripMenuItem.Name = "KRSToolStripMenuItem"
+        Me.KRSToolStripMenuItem.Size = New System.Drawing.Size(134, 36)
+        Me.KRSToolStripMenuItem.Text = "KRS"
+        '
+        'KHSToolStripMenuItem
+        '
+        Me.KHSToolStripMenuItem.Name = "KHSToolStripMenuItem"
+        Me.KHSToolStripMenuItem.Size = New System.Drawing.Size(134, 36)
+        Me.KHSToolStripMenuItem.Text = "KHS"
+        '
+        'LaporanToolStripMenuItem
+        '
+        Me.LaporanToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.CetakKRSToolStripMenuItem, Me.CetakKHSToolStripMenuItem})
+        Me.LaporanToolStripMenuItem.Name = "LaporanToolStripMenuItem"
+        Me.LaporanToolStripMenuItem.Size = New System.Drawing.Size(112, 36)
+        Me.LaporanToolStripMenuItem.Text = "&Laporan"
+        '
+        'CetakKRSToolStripMenuItem
+        '
+        Me.CetakKRSToolStripMenuItem.Name = "CetakKRSToolStripMenuItem"
+        Me.CetakKRSToolStripMenuItem.Size = New System.Drawing.Size(201, 36)
+        Me.CetakKRSToolStripMenuItem.Text = "Cetak KRS"
+        '
+        'CetakKHSToolStripMenuItem
+        '
+        Me.CetakKHSToolStripMenuItem.Name = "CetakKHSToolStripMenuItem"
+        Me.CetakKHSToolStripMenuItem.Size = New System.Drawing.Size(201, 36)
+        Me.CetakKHSToolStripMenuItem.Text = "Cetak KHS"
+        '
+        'SettingToolStripMenuItem
+        '
+        Me.SettingToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PenggunaToolStripMenuItem})
+        Me.SettingToolStripMenuItem.Name = "SettingToolStripMenuItem"
+        Me.SettingToolStripMenuItem.Size = New System.Drawing.Size(103, 36)
+        Me.SettingToolStripMenuItem.Text = "Setting"
+        '
+        'PenggunaToolStripMenuItem
+        '
+        Me.PenggunaToolStripMenuItem.Name = "PenggunaToolStripMenuItem"
+        Me.PenggunaToolStripMenuItem.Size = New System.Drawing.Size(197, 36)
+        Me.PenggunaToolStripMenuItem.Text = "Pengguna"
+        '
+        'BantuanToolStripMenuItem
+        '
+        Me.BantuanToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.InfoAplikasiToolStripMenuItem})
+        Me.BantuanToolStripMenuItem.Name = "BantuanToolStripMenuItem"
+        Me.BantuanToolStripMenuItem.Size = New System.Drawing.Size(115, 36)
+        Me.BantuanToolStripMenuItem.Text = "&Bantuan"
+        '
+        'InfoAplikasiToolStripMenuItem
+        '
+        Me.InfoAplikasiToolStripMenuItem.Name = "InfoAplikasiToolStripMenuItem"
+        Me.InfoAplikasiToolStripMenuItem.Size = New System.Drawing.Size(220, 36)
+        Me.InfoAplikasiToolStripMenuItem.Text = "Info Aplikasi"
+        '
+        'FormUtama
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(12.0!, 25.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.Aplikasi_Akademik.My.Resources.Resources.download
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(1446, 905)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "FormUtama"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Aplikasi Akademik"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Maximized
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents FileToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents JurusanToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents RuanganToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MataKuliahToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents MahasiswaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents DosenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripSeparator
+    Friend WithEvents KeluarToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents TransaksiToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents KRSToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents KHSToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents LaporanToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CetakKRSToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents CetakKHSToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SettingToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents PenggunaToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BantuanToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InfoAplikasiToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+
+End Class
---
+++ Aplikasi_Akademik/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class FormUtama
+    Private Sub KeluarToolStripMenuItem_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles KeluarToolStripMenuItem.Click
+        Dim konfirmasi = MsgBox("Apakah Anda yakin akan keluar Aplikasi?", MsgBoxStyle.Question + MsgBoxStyle.YesNo,
+                                "Konfirmasi")
+
+        If konfirmasi = DialogResult.Yes Then
+            End
+        End If
+
+    End Sub
+End Class
